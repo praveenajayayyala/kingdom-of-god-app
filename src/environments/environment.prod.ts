92ca745c68af8f15f9e56d74ddc1b750d6fc8eae
praveenajayayyala <ajaypraveen@gmail.com>
2025-01-19 13:42:55 -0500

    build budget changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,8 @@ export const environment = {
     baseUrl: "https://so926lyyic.execute-api.ap-south-1.amazonaws.com/prod",
     authorizeUrl: "https://kingdomofgod.auth.ap-south-1.amazoncognito.com/oauth2",
     redirectUrl: "https://www.kingdomofgod.in/admin",
+    redirecPBtUrl: "https://www.kingdomofgod.in/page-builder",
     loginUrl: "https://kingdomofgod.auth.ap-south-1.amazoncognito.com/oauth2/authorize?client_id=vclkssn2ftril9vhgbscnc46r&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone&redirect_uri=https://www.kingdomofgod.in/admin",
+    loginPBUrl: "https://kingdomofgod.auth.ap-south-1.amazoncognito.com/oauth2/authorize?client_id=vclkssn2ftril9vhgbscnc46r&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone&redirect_uri=https://www.kingdomofgod.in/page-builder",
     logoutUrl: "https://kingdomofgod.auth.ap-south-1.amazoncognito.com/logout?client_id=vclkssn2ftril9vhgbscnc46r&logout_uri=https://www.kingdomofgod.in/logout"
   }
